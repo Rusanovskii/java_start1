d23034fb87596190b9661a9e866990ccbb5de975
Rusanovskii <135255747+Rusanovskii@users.noreply.github.com>
2023-07-12 16:33:01 +0300

    Вынес описание тестового стенда в конфигурационный файл
    Реализовал загрузку информации о тестовом стенде из конфигурационного файла.
    
    Реализовал возможность указания при запуске тестов того, какой конфигурационный файл должен использоваться, передавая эту информацию через системные свойства.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.System.setProperty;
@@ -12,17 +16,22 @@ import static org.openqa.selenium.remote.BrowserType.*;
 
 public class ApplicationManager {
     private final String browser;
+    private final Properties properties;
     public WebDriver wd;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser) throws IOException {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (browser.equals(FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(CHROME)) {
@@ -37,12 +46,13 @@ public class ApplicationManager {
         contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         login();
     }
 
     public void login() {
-        sessionHelper.login("admin", "secret");
+
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
